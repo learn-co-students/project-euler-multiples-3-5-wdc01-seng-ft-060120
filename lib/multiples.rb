6f5e6ae3a47fc0c309a95d7e8c230e90374aa6e7
andy-aguilar <aaguil3@gmail.com>
2020-10-08 18:04:10 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,12 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+    array = (1...limit).to_a
+    multiples = array.select {|num| 
+        num % 3 === 0 || num % 5 === 0
+    }
+    return multiples
+end
+
+def sum_multiples(limit)
+    multiples = collect_multiples(limit)
+    return multiples.reduce(:+)
+end
